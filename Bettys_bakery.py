06b8d8edfa1d34eb8c61c9c46477ed37d46dff1e
Rafael <rafaelramosnoriega@gmail.com>
2025-09-02 08:12:11 -0500

    betty's_bakery

---
+++ Bettys_bakery.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+recipes = np.genfromtxt('recipes.csv', delimiter=',')
+
+print(recipes)
+
+print(recipes.shape)
+print(recipes.dtype)
+
+cupcakes = recipes[0]
+print(cupcakes)
+cookies = recipes[2]
+print(cookies)
+
+grocery_list = 2 *cupcakes + cookies
+print(grocery_list) 
